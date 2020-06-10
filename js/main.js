869684870cf56b3731789b5e9610d53d0ff88702
Gabby Suazo <gabby.suazo@gmail.com>
2020-06-10 12:18:56 -0400

    Add javascript structure to main.js and ids to index.html for homepage bubble animation

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// Pink Bubble Hover
+document.getElementById("pink").addEventListener("mouseenter",
+  function() {
+    document.getElementById("text").style.backgroundColor = "#EBBFD0"
+    document.getElementById("text").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  }, false
+);
+
+document.getElementById("pink").addEventListener("mouseleave",
+  function() {
+    document.getElementById("text").style.backgroundColor = "#6A6F7C";
+    document.getElementById("text").innerHTML = "Hover over the services above to learn more."
+  }, false
+);
+
+// Blue Bubble Hover
+document.getElementById("blue").addEventListener("mouseenter",
+  function() {
+    document.getElementById("text").style.backgroundColor = "#7DC6EB"
+    document.getElementById("text").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  }, false
+);
+
+document.getElementById("blue").addEventListener("mouseleave",
+  function() {
+    document.getElementById("text").style.backgroundColor = "#6A6F7C";
+    document.getElementById("text").innerHTML = "Hover over the services above to learn more."
+  }, false
+);
+
+// Red Bubble Hover
+document.getElementById("red").addEventListener("mouseenter",
+  function() {
+    document.getElementById("text").style.backgroundColor = "#CF6060"
+    document.getElementById("text").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  }, false
+);
+
+document.getElementById("red").addEventListener("mouseleave",
+  function() {
+    document.getElementById("text").style.backgroundColor = "#6A6F7C";
+    document.getElementById("text").innerHTML = "Hover over the services above to learn more."
+  }, false
+);
